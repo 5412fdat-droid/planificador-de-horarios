05cb4c6ce14240f0261b0eef0f562248a20ec499
Usuario <test@example.com>
2026-06-26 10:41:07 -0400

    Revert "Inject local database dump to Railway via loaddata"
    This reverts commit 15a45a0c9cdbe8aa0664099f4def4ff7116e2b51.

--- backend/manage.py
+++ backend/manage.py
@@ -15,6 +15,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    if len(sys.argv) > 1 and sys.argv[1] == 'migrate':
+        print("Skipping automatic migrations to prevent already exists errors.")
+        return
     execute_from_command_line(sys.argv)
 
 
