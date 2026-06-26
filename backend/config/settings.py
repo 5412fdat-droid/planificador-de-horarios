f0f792a3e285854f22008f5710a7701adac802ed
Usuario <test@example.com>
2026-06-26 05:58:23 -0400

    Remove debug code

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -14,8 +14,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
-raise Exception("ENV_KEYS: " + str(list(os.environ.keys())))
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
